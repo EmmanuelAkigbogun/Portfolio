251448c9e6d9779caa36c7edd7cc038cf158fb97
EmmanuelAkigbogun <105459628+EmmanuelAkigbogun@users.noreply.github.com>
2026-06-02 20:26:16 +0100

    Add files via upload

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,10 +1,57 @@
 // src/assets/images.js
-import heroMockup from "./dark.png"; // Make sure this file exists in src/assets/
+
+// --- Core Layout Assets ---
+import heroMockup from "./dark.png";
 import livehocky from "./draw.png";
 
+// --- Base Pattern Assets (1 to 13) ---
+import draw1p from "./draw (1).png";
+import draw1j from "./draw (1).jpg";
+// import draw2p from "./draw (2).png";
+import draw2j from "./draw (2).jpg";
+import draw3p from "./draw (3).png";
+import draw3j from "./draw (3).jpg";
+// import draw4p from "./draw (4).png";
+import draw4j from "./draw (4).jpg";
+// import draw5p from "./draw (5).png";
+import draw5j from "./draw (5).jpg";
+// import draw6p from "./draw (6).png";
+import draw6j from "./draw (6).jpg";
+// import draw7p from "./draw (7).png";
+import draw7j from "./draw (7).jpg";
+// import draw8p from "./draw (8).png";
+import draw8j from "./draw (8).jpg";
+// import draw9p from "./draw (9).png";
+import draw9j from "./draw (9).jpg";
+// import draw10p from "./draw (10).png";
+import draw10j from "./draw (10).jpg";
+// import draw11p from "./draw (11).png";
+import draw11j from "./draw (11).jpg";
 export const Images = {
   hero: heroMockup,
   draw: livehocky,
-  // You can add more here later:
-  // artistry: artistryImg,
+
+  // Pattern Iteration Array Mappings
+  draw1p,
+  draw1j,
+  // draw2p,
+  draw2j,
+  draw3p,
+  draw3j,
+  // draw4p,
+  draw4j,
+  // draw5p,
+  draw5j,
+  // draw6p,
+  draw6j,
+  // draw7p,
+  draw7j,
+  // draw8p,
+  draw8j,
+  // draw9p,
+  draw9j,
+  // draw10p,
+  draw10j,
+  // draw11p,
+  draw11j,
 };
